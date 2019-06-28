ce2c3228918674bf675cd4f92b29ada671504c9b
TextError <savinraul1983@yahoo.com>
2019-06-28 15:22:50 +0100

    MUI grid

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// MUI
+import Grid from '@material-ui/core/Grid';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        Home
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={6} xs={12}>
+          <p>Content ...</p>
+        </Grid>
+        <Grid item sm={6} xs={12}>
+          <p>Content ...</p>
+        </Grid>
+      </Grid>
     )
   }
 }
