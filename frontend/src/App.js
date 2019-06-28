792ba986d3386c8a110c6e7fa5873b16d48e6bb6
TextError <savinraul1983@yahoo.com>
2019-06-28 14:26:31 +0100

    material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
+import Home from './components/home/Home';
+import Register from './components/authentication/register/Register';
+import Login from './components/authentication/login/Login';
 
 // Css
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
