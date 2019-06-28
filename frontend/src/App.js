37d897e1ec52bbda3d687630b03463de5fd1221f
TextError <savinraul1983@yahoo.com>
2019-06-28 14:50:25 +0100

    fast navbar links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,27 @@ import Home from './components/home/Home';
 import Register from './components/authentication/register/Register';
 import Login from './components/authentication/login/Login';
 
+// Layout
+import Navbar from './Layout/navbar/Navbar';
+
 // Css
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-      </Switch>
-    </Router>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
