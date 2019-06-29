195c0d4bb969796237fc1e28172ff5d09076a99d
TextError <savinraul1983@yahoo.com>
2019-06-29 18:52:09 +0100

    finish later

--- frontend/src/components/authentication/register/Register.js
+++ frontend/src/components/authentication/register/Register.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
 // MUI
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 
 // Components
@@ -30,89 +28,95 @@ class Register extends Component {
   render() {
     const { first_name, last_name, email, password, password2 } = this.state;
     return (
-      <div className='register'>
-        <form className='' noValidate>
-          <Grid 
-            container
-            spacing={0}
-            alignItems="center"
-            justify="center"
-            style={{ minHeight: '100vh' }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <InputField
-                  variant='outlined'
-                  id="first_name"
-                  label="First Name"
-                  name="first_name"
-                  value={first_name}
-                  autoComplete="first_name"
-                  onChange={this.onChange}
-                /> 
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <InputField
-                  variant='outlined'
-                  id="last_name"
-                  label="Last Name"
-                  name="last_name"
-                  value={last_name}
-                  autoComplete="last_name"
-                  onChange={this.onChange}
-                /> 
-              </Grid>
-              <Grid item xs={12}>
-                <InputField
-                  variant='outlined'
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  value={email}
-                  autoComplete="email"
-                  onChange={this.onChange}
-                /> 
-              </Grid>
-              <Grid item xs={12}>
-                <InputField
-                  variant='outlined'
-                  id="password"
-                  label="Password"
-                  name="password"
-                  value={password}
-                  autoComplete="password"
-                  onChange={this.onChange}
-                /> 
-              </Grid>
-              <Grid item xs={12}>
-                <InputField
-                  variant='outlined'
-                  id="password2"
-                  label="Confirm Password"
-                  name="password2"
-                  value={password2}
-                  autoComplete="password2"
-                  onChange={this.onChange}
-                /> 
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-            >
-              Sign Up
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-        </form>
+      <div className='register mt-5'>
+        <div className='container'>
+          <div className='row no-gutters'>
+            <div className='col-md-6 mx-auto'>
+              <div className='card'>
+                <div className="card-header text-white">
+                  <h4>Register</h4>
+                </div>
+                <div className="card-body">
+                  <form noValidate onSubmit={this.onSubmit}>
+                    <div className='row no-gutters'>
+                      <div className='col'>
+                        <InputField
+                          variant='outlined'
+                          id="first_name"
+                          label="First Name"
+                          name="first_name"
+                          value={first_name}
+                          autoComplete="first_name"
+                          onChange={this.onChange}
+                        /> 
+                        <InputField
+                          variant='outlined'
+                          id="last_name"
+                          label="First Name"
+                          name="last_name"
+                          value={last_name}
+                          autoComplete="last_name"
+                          onChange={this.onChange}
+                        /> 
+                      </div>
+                    </div>
+                    <div className='row no-gutters'>
+                      <div className='col'>
+                        <InputField
+                          variant='outlined'
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          value={email}
+                          autoComplete="email"
+                          onChange={this.onChange}
+                        /> 
+                      </div>
+                    </div>
+                    <div className='row no-gutters'>
+                      <div className='col'>
+                        <InputField
+                          variant='outlined'
+                          id="password"
+                          label="Password"
+                          name="password"
+                          value={password}
+                          autoComplete="password"
+                          onChange={this.onChange}
+                        /> 
+                      </div>
+                    </div>
+                    <div className='row no-gutters'>
+                      <div className='col'>
+                        <InputField
+                          variant='outlined'
+                          id="password2"
+                          label="Confirm Password"
+                          name="password2"
+                          value={password2}
+                          autoComplete="password2"
+                          onChange={this.onChange}
+                        /> 
+                      </div>
+                    </div>
+                    <div className='row no-gutters'>
+                      <div className='col'>
+                        <Button
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                        >
+                          Sign Up
+                        </Button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
